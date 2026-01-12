643835c3890c38e4a575414fef9e7b68632b59d4
jeremybrown2021 <jeremy@designspartans.com>
2026-01-12 23:50:33 +0500

    fix: update package.json and layout.tsx for improved Bootstrap integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import "../styles/font-montserrat.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../styles/style.css";
@@ -33,16 +34,6 @@ export default function RootLayout({ children }) {
             </head>
             <body>
                 {children}
-                <script
-                    src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-                    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-                    crossOrigin="anonymous"
-                ></script>
-                <script
-                    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-                    integrity="sha384-..."
-                    crossOrigin="anonymous"
-                ></script>
             </body>
         </html>
     );
